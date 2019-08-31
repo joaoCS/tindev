f2ddd9933d25a18fedab26e509ca5fb9d5b14fb6
joao <joaoantoniogba025@gmail.com>
2019-08-31 11:27:57 -0300

    Primeiro e único commit

---
+++ backend/src/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+const server = require('http').Server(app);
+
+const io = require('socket.io')(server);
+
+const connectedUsers = {};
+
+
+io.on('connection', socket => {
+    const { user } = socket.handshake.query;
+
+    console.log(user, socket.id)
+
+    connectedUsers[user] = socket.id
+});
+
+
+mongoose.connect("mongodb+srv://thisis:thisis@cluster0-zpfz0.mongodb.net/test?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+});
+
+
+
+app.use((req, res, next) => {
+    req.io  = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+})
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+server.listen(3333);
